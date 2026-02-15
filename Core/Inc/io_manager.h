48a382e60034f941af6ad7dc8690dbe38e8c5588
Adi <adisatheesh4@gmail.com>
2026-02-15 00:17:58 -0500

    Add IO reading logic

---
+++ Core/Inc/io_manager.h
@@ -0,0 +1,35 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : io_manager.h
+  * @brief          : IO manager header
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+#include "main.h"
+#include "io_data.h"
+#include "stm32l4xx_hal.h"
+
+/**
+  * @brief  Initialize IO manager
+  * @retval HAL_StatusTypeDef
+  */
+HAL_StatusTypeDef IO_Manager_Init(void);
+
+/**
+  * @brief  Main IO manager task
+  * @param  argument: Not used
+  * @retval None
+  */
+void IO_ManagerTask(void *argument);
